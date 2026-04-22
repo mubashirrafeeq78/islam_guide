122f0618054f9be6b302097f96a84b2a45a99933
mubashirrafeeq78 <officialmubashirhassan@gmail.com>
2026-04-22 11:46:49 +0500

    Refactor app to use WebView instead of InAppWebView

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  
-  // ایپ شروع ہونے سے پہلے ضروری پرمیشنز چیک کرنا
-  await _requestInitialPermissions();
-
+void main() {
   runApp(const MaterialApp(
+    home: NoorAppHome(),
     debugShowCheckedModeBanner: false,
-    home: NoorAppWebView(),
   ));
 }
 
-// کیمرہ اور میڈیا پرمیشنز مانگنے کا فنکشن
-Future<void> _requestInitialPermissions() async {
-  await [
-    Permission.camera,
-    Permission.photos,
-    Permission.microphone,
-  ].request();
-}
-
-class NoorAppWebView extends StatefulWidget {
-  const NoorAppWebView({super.key});
+class NoorAppHome extends StatefulWidget {
+  const NoorAppHome({super.key});
 
   @override
-  State<NoorAppWebView> createState() => _NoorAppWebViewState();
+  State<NoorAppHome> createState() => _NoorAppHomeState();
 }
 
-class _NoorAppWebViewState extends State<NoorAppWebView> {
-  InAppWebViewController? webViewController;
+class _NoorAppHomeState extends State<NoorAppHome> with WidgetsBindingObserver {
+  late final WebViewController _controller;
+  final String _appUrl = "https://lavenderblush-eagle-882875.hostingersite.com/dashboard.php";
+
+  @override
+  void initState() {
+    super.initState();
+    // ایپ کی لائف سائیکل پر نظر رکھنے کے لیے آبزرور کا آغاز
+    WidgetsBinding.instance.addObserver(this);
+
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+        ),
+      )
+      ..loadRequest(Uri.parse(_appUrl));
+  }
+
+  @override
+  void dispose() {
+    // آبزرور کو ختم کرنا
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  // یہ فنکشن چیک کرتا ہے کہ ایپ کی حالت کب بدلی
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      // جیسے ہی ایپ دوبارہ اوپن ہوگی، ڈومین خود بخود ری لوڈ ہو جائے گا
+      // اس سے ویب سائٹ دوبارہ سیکیورٹی پن مانگے گی
+      _controller.reload();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // اوپر سے اسٹیٹس بار کو صاف رکھنے کے لیے SafeArea
+      // اوپر کا حصہ (Status Bar) کو محفوظ رکھنے کے لیے SafeArea
       body: SafeArea(
-        child: InAppWebView(
-          initialUrlRequest: URLRequest(
-            url: WebUri("https://lavenderblush-eagle-882875.hostingersite.com/dashboard.php"),
-          ),
-          initialSettings: InAppWebViewSettings(
-            javaScriptEnabled: true,
-            useOnDownloadStart: false, // ڈاؤن لوڈنگ سختی سے بند ہے
-            allowsInlineMediaPlayback: true,
-            safeBrowsingEnabled: true,
-            // میڈیا اپ لوڈنگ اور کیمرہ کے لیے ضروری سیٹنگ
-            allowFileAccessFromFileURLs: true,
-            allowUniversalAccessFromFileURLs: true,
-          ),
-          onWebViewCreated: (controller) {
-            webViewController = controller;
-          },
-          // ویب سائٹ کے اندر پرمیشن کی درخواستوں کو ہینڈل کرنا (کیمرہ/مائیک)
-          onPermissionRequest: (controller, request) async {
-            return PermissionResponse(
-              resources: request.resources,
-              action: PermissionResponseAction.GRANT,
-            );
-          },
-          // واٹس ایپ اور بیرونی لنکس کو ہینڈل کرنا
-          shouldOverrideUrlLoading: (controller, navigationAction) async {
-            var uri = navigationAction.request.url!;
-            if (!["http", "https", "file", "chrome", "data", "javascript"].contains(uri.scheme)) {
-              if (await canLaunchUrl(uri)) {
-                await launchUrl(uri, mode: LaunchMode.externalApplication);
-                return NavigationActionPolicy.CANCEL;
-              }
-            }
-            return NavigationActionPolicy.ALLOW;
-          },
-        ),
+        child: WebViewWidget(controller: _controller),
       ),
     );
   }
