8575aa15f72e35587b403589bd509afa7b65dd01
mubashirrafeeq78 <officialmubashirhassan@gmail.com>
2026-04-23 09:09:29 +0500

    Improve comments and clean up MainActivity
    Updated comments for clarity and removed unused code.

--- android/app/src/main/kotlin/com/masail/hal/islam_guide/MainActivity.kt
+++ android/app/src/main/kotlin/com/masail/hal/islam_guide/MainActivity.kt
@@ -7,23 +7,13 @@ import io.flutter.embedding.android.FlutterActivity
 class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // ریسنٹ ایپس میں ڈیٹا چھپانے کے لیے
+        // ریسنٹ ایپس میں ڈیٹا چھپانے اور اسکرین شاٹ بلاک کرنے کے لیے
         window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
     }
 
     override fun onResume() {
         super.onResume()
-        // سیکیورٹی برقرار رکھنے کے لیے فلیگ دوبارہ سیٹ کریں
+        // سیکیورٹی فلیگ کو دوبارہ یقینی بنانا
         window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
-        
-        // یہ لائن فلٹر انجن کو ریفریش کرنے میں مدد دیتی ہے
-        // جب ایپ دوبارہ اوپن ہوگی تو یہ ویب ویو کو دوبارہ لوڈ کرنے کا سگنل دے گی
-        flutterEngine?.navigationControlSurface?.onUserLeaveHint()
-    }
-
-    // یہ فنکشن ایمرجنسی میں کام آتا ہے
-    override fun onUserLeaveHint() {
-        super.onUserLeaveHint()
-        // جب یوزر ہوم بٹن دبائے گا، تو یہ ایپ کی میموری کو تازہ کر دے گا
     }
 }
